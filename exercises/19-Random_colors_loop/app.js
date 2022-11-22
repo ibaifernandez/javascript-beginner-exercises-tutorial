fee715d4a81091e6ac7ab3be8226568f667bcd71
Ibai Fernández <info@ibaifernandez.com>
2022-11-22 21:19:10 +0000

    Trying to get the Ex. #19 test to work 😤

--- exercises/19-Random_colors_loop/app.js
+++ exercises/19-Random_colors_loop/app.js
@@ -1,31 +1,3 @@
-/*
-
-function getColor(colorNumber) {
-	switch (colorNumber) {
-	  case 1: return "red";
-		  case 2: return "yellow";
-		  case 3: return "blue";
-		  case 4: return "green";
-		  default: return "black";
-	}
-  }
-  
-  function getAllStudentColors(students) {
-	var students = prompt("How many students you need to assign a color to?");
-	students = parseInt(students);
-	var colorPerStudent = {};
-	
-	for (let i = 0; i < students; i++) {
-	  chosenColor = getColor(Math.floor(Math.random() * 5));
-	  colorPerStudent[`Student #${[i+1]}`] = chosenColor;
-	}
-	return colorPerStudent;
-  }
-  
-  console.log(getAllStudentColors());
-
-*/
-
 function getColor(colorNumber) {
 	switch (colorNumber) {
 	  case 1: return "red";
@@ -40,36 +12,7 @@ function getAllStudentColors() {
 	let students = parseInt(prompt("How many students do you wish to choose a color for?"));
 	
 	for (let i = 0; i < students; i++) {
-	  chosenColor = getColor(Math.floor(Math.random() * 5));
-	  console.log(chosenColor)
-	}
-	return "This is a function without a proper return.";
-  }
-  
-/*
-
-function getColor(colorNumber) {
-	switch (colorNumber) {
-	  case 1: return "red";
-		  case 2: return "yellow";
-		  case 3: return "blue";
-		  case 4: return "green";
-		  default: return "black";
+	  let chosenColor = getColor(Math.floor(Math.random() * 5));
+	  console.log(chosenColor);
 	}
   }
-
-function getAllStudentColors("10") {
-  students = parseInt(students);
-    if (students > 0) {
-    chosenColor = getColor(Math.floor(Math.random() * 5));
-    students--;
-    getAllStudentColors(students);
-  }
-  return "The chosen colors for each student have just been printed."
-}
-  
-
-//call the function below with the number of students in the class and print on the console
-getAllStudentColors(10);
-
-*/
